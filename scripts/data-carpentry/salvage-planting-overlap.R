8e8e450951daaec26b776fc5730a0e3133aabe95
Derek Young <derek.j.n.young@gmail.com>
2017-06-14 08:58:56 -0700

    Expand definition of salvage; use date accomplished instead of completed

--- scripts/data-carpentry/salvage-planting-overlap.R
+++ scripts/data-carpentry/salvage-planting-overlap.R
@@ -2,10 +2,9 @@ setwd("~/UC Davis/Research Projects/Post-fire management/postfire-management")
 
 library(sf)
 
-
 ## Load FACTS data
 facts <- st_read(dsn = "data/non-synced/existing-datasets/CA_Activity_merged.shp", stringsAsFactors = FALSE)
-facts$year <- substr(facts$DATE_COMPL,1,4)
+facts$year <- substr(facts$DATE_ACCOM,1,4)
 #project to CA Albers
 facts <- st_transform(facts,crs=3310)
 facts$id <- 1:nrow(facts)
@@ -16,27 +15,27 @@ planting <- c("Plant Trees")
 facts.planting <- facts[facts$ACTIVITY %in% planting,]
 
 ## Make salvage-only FACTS layer
-salvage <- c("Salvage Cut (intermediate treatment, not regeneration)")
+salvage <- c("Salvage Cut (intermediate treatment, not regeneration)","Stand Clearcut (EA/RH/FH)")
 facts.salvage <- facts[facts$ACTIVITY %in% salvage,]
 
 ## We're only interested in areas planted between 2000 and 2014
 planting.years <- 2000:2014
 facts.planting <- facts.planting[facts.planting$year %in% planting.years,]
 
-## We're only interested in salvage that happened within 8 years prior to the planting
-salvage.prior <- 8
+## We're only interested in salvage that happened within 6 years prior to the planting
+salvage.prior <- 6
 salvage.years <- (min(planting.years)-salvage.prior):(max(planting.years))
 facts.salvage <- facts.salvage[facts.salvage$year %in% salvage.years,]
 
 
-## For each planted polygon, get all touching salvage polygons from up to 10 years prior to the planting
+## For each planted polygon, get all touching salvage polygons from up to 6 years prior to the planting
 all.overlap.ids <- NULL #just add first row as an example
 
 for(i in 1:nrow(facts.planting)) {
   
   facts.planting.polygon <- facts.planting[i,]
   planting.year <- as.numeric(facts.planting.polygon$year)
-  salvage.year.range <- (planting.year-10):planting.year
+  salvage.year.range <- (planting.year-10):planting.year #this includes salvage that happened in the same year as planting
   facts.salvage.matchyears <- facts.salvage[facts.salvage$year %in% salvage.year.range,]
   
   # see which salvage polygons (rows) at least partially overlap the planted areas
@@ -58,10 +57,6 @@ overlapping.salvage.polygons <- facts.salvage[facts.salvage$id %in% all.overlap.
 
 
 ## Save the planting and salvage polygons
-
-
-
 st_write(facts.planting,dsn="data/output-exploratory/salvage-overlap-planting/planting.shp",delete_dsn=TRUE)
 st_write(overlapping.salvage.polygons,dsn="data/output-exploratory/salvage-overlap-planting/salvage_that_overlaps.shp",delete_dsn=TRUE)
-
-st_write(facts.salvage,dsn="data/output-exploratory/salvage-overlap-planting/salvage_all.shp")
+#st_write(facts.salvage,dsn="data/output-exploratory/salvage-overlap-planting/salvage_all.shp") # for testing
