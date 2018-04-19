cec8f75cb644d77e1219c378412cd8f78e702d86
Unknown <derek.j.n.young@gmail.com>
2018-04-19 09:14:54 -0700

    Transfer functions to separate function script

--- scripts/site-selection/plot_stratification_spatial.R
+++ scripts/site-selection/plot_stratification_spatial.R
@@ -11,17 +11,11 @@ library(stringr)
 library(rgeos)
 library(DT)
 
+source("scripts/site-selection/plot_stratification_spatial_functions.R")
+
 crs <- 3310 # CA albers
 
 set.seed(1)
-
-st_drop_geometry <- function(x) {
-  if(inherits(x,"sf")) {
-    x <- st_set_geometry(x, NULL)
-    class(x) <- 'data.frame'
-  }
-  return(x)
-}
 
 
 
@@ -53,61 +47,6 @@ d.trt <- d.trt %>%
 ## NOTE that some internal plots that were salvaged are in the database twice if needed to match with the salvage categories "both" and "planted"
 
 #### 1. Narrow the environmental space based on the intersection of env. space represented by each planting year (near to seed source only)  ####
-
-#function to find outliers
-# an outlier is a point with a value so extreme (high or low) that it has a < 1% chance of being observed, given the size of the dataset and assuming a normal distribution
-outliers <- function(x) {
-  mean <- mean(x)
-  sd <- sd(x)
-  x.dev <- abs(x-mean)
-  prob <- pnorm(x.dev,0,sd,lower.tail=FALSE)
-
-  threshold <- 1/(length(x)*3)
-  
-  outlier <- (prob < threshold)
-  return(outlier)
-}
-
-#possible future alternative outliers alg: make sure there are at least 5% of the total number of plots (or, at least 3 plots) within 10% of the range of variable values of the focal plot
-
-# function to find the range of elev and rad encompassed by all focal plots
-plt.yr.env.range <- function(plt.yr,plots) {
-  
-  fire <- plots$fire2[1]
-  
-  # filter to the current planting year
-  plots.yr <- plots %>%
-    filter(yr.pltd == plt.yr &
-             dist.non.high < 80) # and when computing range, only look at plots close to seed source (this means only looking at perimeter plots, because previously we filtered out the internal plots close to seed source)
-  
-  # find the non-outliers of elevation and radiation
-  elevs <- plots.yr$elev
-  elevs.keep <- elevs[!outliers(elevs)]
-  elevs.outliers <- elevs[outliers(elevs)]
-  elev.low <- min(elevs.keep)
-  elev.high <- max(elevs.keep)
-  
-  if(length(elevs.outliers) > 0) {
-    cat("\nOn",fire,"planting year",plt.yr,"removed elev outliers:",elevs.outliers)
-  }
-  
-  rads <- plots.yr$rad
-  rads.keep <- rads[!outliers(rads)]
-  rads.outliers <- rads[outliers(rads)]
-  rad.low <- min(rads.keep)
-  rad.high <- max(rads.keep)
-  
-  if(length(rads.outliers) > 0) {
-    cat("\nOn",fire,"planting year",plt.yr,"removed rad outliers:",rads.outliers)
-  }
-  
-  env.range.yr <- data.frame(plt.yr,elev.low,elev.high,rad.low,rad.high)
-  row.names(env.range.yr) <- NULL
-    
-  return(env.range.yr)
-  
-}
-
 
 ### Moontelope ###
 
@@ -182,35 +121,6 @@ rad.low <- focal.range$rad.low
 rad.high <- focal.range$rad.high
 elev.low <- focal.range$elev.low
 elev.high <- focal.range$elev.high
-
-define.quadrants <- function(rad.low.arg,rad.high.arg,elev.low.arg,elev.high.arg,rad.cats.arg,elev.cats.arg) {
-
-  # create radiation and elev breaks
-  rad.breaks <- seq(from=rad.low.arg,to=rad.high.arg,length.out=rad.cats.arg+1)
-  elev.breaks <- seq(from=elev.low.arg,to=elev.high.arg,length.out=elev.cats.arg+1)
-  
-  # put into data frame form
-  rad.matrix <- cbind(rad.low = rad.breaks[-(rad.cats.arg+1)],
-                      rad.high = rad.breaks[-1]) %>%
-    as.data.frame()
-  rad.matrix$rad.label <- paste0("R",1:nrow(rad.matrix))
-  
-  elev.matrix <- cbind(elev.low = elev.breaks[-(elev.cats.arg+1)],
-                      elev.high = elev.breaks[-1]) %>%
-    as.data.frame()
-  elev.matrix$elev.label <- paste0("E",1:nrow(elev.matrix))
-  
-  # merge the two data frames
-  # first replicate the rad data frame the number of times equal to the number of rows in the elev data frame
-  rad.matrix.rep <- rad.matrix[rep(row.names(rad.matrix),each=nrow(elev.matrix)),]
-  elev.matrix.rep <- elev.matrix[rep(row.names(elev.matrix),times=nrow(rad.matrix)),]
-  
-  env.cats <- bind_cols(rad.matrix.rep,elev.matrix.rep)
-  env.cats$label <- paste0(env.cats$elev.label,env.cats$rad.label)
-  
-  return(env.cats)
-  
-}
 
 env.quads <- define.quadrants(rad.low,rad.high,elev.low,elev.high,rad.cats,elev.cats)
 
@@ -335,107 +245,6 @@ suid.summ <- d.foc.yr.classified %>%
 
 
 ### Now the goal is to find the smallest number of SUIDS that can fill at least 2 subquads of every quad
-
-##function to compute the quality of stratification given a tally of how many plots are in each sub-quad
-score.stratif <- function(sub.quads.df,simp=FALSE) {
-  
-  # count the number of subquads filled
-  n.sub.quads.filled <- sum(sub.quads.df$nplots>0,na.rm=TRUE)
-  
-  # count the number of quadrants with 2+ subquads filled
-  quads <- sub.quads.df %>%
-    group_by(quad.label) %>%
-    summarize(n.subquads.full = sum(nplots >= 1,na.rm=TRUE)) %>%
-    mutate(two.plus.subquads.full = n.subquads.full >= 2) %>%
-    ungroup()
-  n.quads.w.two.plus.subquads <- sum(quads$two.plus.subquads.full,na.rm=TRUE)
-  
-  # count the number of quadrants with 2+ subquads double-filled
-  quads <- sub.quads.df %>%
-    group_by(quad.label) %>%
-    summarize(n.subquads.full = sum(nplots >= 2,na.rm=TRUE)) %>%
-    mutate(two.plus.subquads.full = n.subquads.full >= 2) %>%
-    ungroup()
-  n.quads.w.two.plus.subquads.double <- sum(quads$two.plus.subquads.full,na.rm=TRUE)
-  
-  # count the number of quadrants with 2+ subquads triple-filled
-  quads <- sub.quads.df %>%
-    group_by(quad.label) %>%
-    summarize(n.subquads.full = sum(nplots >= 3,na.rm=TRUE)) %>%
-    mutate(two.plus.subquads.full = n.subquads.full >= 2) %>%
-    ungroup()
-  n.quads.w.two.plus.subquads.triple <- sum(quads$two.plus.subquads.full,na.rm=TRUE)
-  
-  ret <- list(n.sub.quads.filled=n.sub.quads.filled,quads=quads,n.quads.w.two.plus.subquads=n.quads.w.two.plus.subquads,n.quads.w.two.plus.subquads.double=n.quads.w.two.plus.subquads.double,n.quads.w.two.plus.subquads.triple=n.quads.w.two.plus.subquads.triple)
-  
-  if(simp==TRUE) {
-    #return only the "n.sub.quads.filled" and "n.quads.w.two.plus.subquads"
-    ret <- ret[c("n.sub.quads.filled","n.quads.w.two.plus.subquads","n.quads.w.two.plus.subquads.double","n.quads.w.two.plus.subquads.triple")]
-    ret <- as.data.frame(ret)
-  }
-  
-  return(ret)
-  
-}
-
-
-### function to take a SUID and compute what the sub.quads.df (stratification records) would be if it were added
-add.suid <- function(suid,d.foc.yr.classified,sub.quads.df,tier=0) {
-  
-  d.foc.yr.classified.suid <- d.foc.yr.classified %>%
-    filter(f.s.first.planting.suid == suid) %>% # look only at the SUID in question
-    group_by(overall.label) %>% 
-    summarize(nplots.added = n()) %>% # compute the number of plots within each subquadrant
-    ungroup()
-  
-  ##add the new counts to the stratification records: first as a new column
-  new.sub.quads.df <- left_join(sub.quads.df,d.foc.yr.classified.suid,by="overall.label")
-  new.sub.quads.df[is.na(new.sub.quads.df$nplots.added),"nplots.added"] <- 0
-  
-  # then replace the original "nplots" column
-  new.sub.quads.df <- new.sub.quads.df %>%
-    mutate(nplots = nplots + nplots.added)
-    
-  # then if tier is 1, add to nplots.tier1
-  if(tier==1) {
-    new.sub.quads.df <- new.sub.quads.df %>%
-      mutate(nplots.tier1 = nplots.tier1 + nplots.added)
-  }
-  
-  # then if tier is 2, add to nplots.tier2
-  if(tier==2) {
-    new.sub.quads.df <- new.sub.quads.df %>%
-      mutate(nplots.tier2 = nplots.tier2 + nplots.added)
-  }
-  
-  # then if tier is 3, add to nplots.tier3
-  if(tier==3) {
-    new.sub.quads.df <- new.sub.quads.df %>%
-      mutate(nplots.tier3 = nplots.tier3 + nplots.added)
-  }
-  
-  # finally, remove the temporary "nplots.added" column
-  new.sub.quads.df <- new.sub.quads.df %>%
-    dplyr::select(-nplots.added)
-    
-  return(new.sub.quads.df)
-  
-}
-
-## function to take a SUID and calculate the new stratification scores (simplified) if it were added
-strat.scores.w.new.suid <- function(suid,sub.quads.df,d.foc.yr.classified) {
-  
-  #get the new sub quads DF if the SUID were added
-  new.sub.quads.df <- add.suid(suid,d.foc.yr.classified,sub.quads.df)
-  
-  #score it
-  strat.score <- score.stratif(new.sub.quads.df,simp=TRUE)
-  
-  return(strat.score)
-}
-
-
-
 
 # for keeping track of which SUIDs we still have as candidates to add
 # initialize it with all SUIDS that contain points of the focal management category
@@ -638,7 +447,11 @@ suids.remaining
 sub.quads.df
 suids.selected
 
-####$$$ In sub.quads.df, nplots.tier1 is the number of plots within that subquad that were selected in the first tier of stratifiction (that is, finding SUIDS that filled at lesat 2 subquads of every quad at least once). In order to minimize the number of SUIDs we survey: In each quad, our candidates are subquads that have a least 3 nplots. If none have at least 3 nplots, 2 nplots is OK. Then, for each of those, choose the top two subquads in terms of nplots.tier1. If none of those has at least 3 plots, select those that would maximize the total sum of nplots tier1 + tier2. then if not that, tier1 + tier2 + tier3
+####$$$ In sub.quads.df, nplots.tier1 is the number of plots within that subquad that were selected in the first tier of stratifiction
+# that is, finding SUIDS that filled at lesat 2 subquads of every quad at least once). In order to minimize the number of SUIDs we survey:
+# In each quad, our candidates are subquads that have a least 3 nplots. If none have at least 3 nplots, 2 nplots is OK.
+# Then, for each of those, choose the top two subquads in terms of nplots.tier1. If none of those has at least 3 plots,#
+#select those that would maximize the total sum of nplots tier1 + tier2. then if not that, tier1 + tier2 + tier3
 
 
 # and if so update the strat records
