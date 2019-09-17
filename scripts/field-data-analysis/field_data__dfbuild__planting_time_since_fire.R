60073f5856acf8b800dfe3cb9b3b04818124c133
Quinn Sorenson <qmsorenson@gmail.com>
2019-09-17 15:50:34 -0700

    Added Precip to figures

--- scripts/field-data-analysis/field_data__dfbuild__planting_time_since_fire.R
+++ scripts/field-data-analysis/field_data__dfbuild__planting_time_since_fire.R
@@ -3,21 +3,6 @@ library(gridExtra)
 library(qpcR)
 library(tidyverse)
 
-#####  Buildup of dataframes for the folloing analyses ---------------------------------------------
-
-#  dens.all ~ Year since fire 
-#  dens.pine  ~ Year since fire
-#  dens.PIPO  ~ Year since fire
-#  dens.PIJE ~ Year since fire
-#  ave.ht.all ~ Year since fire 
-#  ave.ht.pine  ~ Year since fire
-#  ave.ht.PIPO  ~ Year since fire
-#  ave.ht.PIJE ~ Year since fire
-#  mass.all ~ Year since fire 
-#  mass.pine  ~ Year since fire
-#  mass.PIPO  ~ Year since fire
-#  mass.PIJE ~ Year since fire
-
 
 #####  load data -----------------------------------------------------------------------------------
 
@@ -31,7 +16,6 @@ seedlings_plot <- seedlings_plot %>%
 
 
 ##### make summaries of each variable --------------------------------------------------------------
-
 
 # group species
 seedlings_plot <- seedlings_plot %>%
@@ -80,8 +64,6 @@ seedl_dhm <- seedlings_plot %>%
             tot.mass.oak = sum(TotHeight^2 * DensitySlope*oak),
             tot.mass.conif = sum(TotHeight^2 * DensitySlope*conif))
  
-
-
 # combine with plot data
 plot_dhm <- plots %>%
   mutate(fsplanted = ifelse(!is.na(facts.planting.first.year), "planted", "unplanted")) %>% # create a variable for whether were planted or not
@@ -96,53 +78,19 @@ plot_dhm_long <- plot_dhm %>%
   mutate(value = ifelse(is.na(value), 0, value)) %>% #assign 0 to na values
   mutate(met.type = str_replace(metric, "\\.[^.]+$" , ""))
 
-
-## plot distribution of response vars by fire and treated status
-explore <- ggplot(plot_dhm_long, aes(x=interaction(Type, Fire), y=log(value+1),color=Type)) +
-  geom_violin(size = 1) +
-  #geom_boxplot(width=0.1) +
-  #geom_jitter(width = .02, alpha = .25) +
-  geom_dotplot(binaxis='y', method = "histodot", stackdir='center',binwidth = .2, dotsize=.7,  aes(x = interaction(Type, Fire), fill = Type)) +
-  #geom_point(position=position_jitterdodge(dodge.width=0.5,jitter.width=0.15),size=3) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme_bw(12) +
-  facet_wrap(~metric,scales="free")
-ggsave(explore, file = "C:/Users/Quinn Sorenson/Desktop/lotsOplots.pdf", width = 60, height = 40, limitsize = FALSE)
+save(plot_dhm_long, file= "output/plotSeedlingData.RData")
 
 
-##### Create function to plot vars and apply to all var combinations -------------------------------------------------
-
-enviroPlots <- function(met, enviro, trans = "none") {
-  assign(paste0(enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met }), ggplot(plot_dhm_long %>% filter(met.type == met), 
-                                                                                           aes(x = get(enviro), y = if (trans == "log") { log(value+1) } 
-                                                                                               else { value }, color = fsplanted)) +
-  geom_smooth(method = "lm", size = 1.5) +
-  geom_point(size = 2) +
-  ylab(if (trans == "log") { paste0("log.", met) } else { met }) +
-  scale_color_manual(values = c("#D5C332", "#048BA8")) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme_bw(12) +
-  facet_wrap(~Fire*metric, scales="free"))
-ggsave(get(paste0(enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met })), 
-       file = paste0("figures/exploratory/", enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met }, ".pdf"), 
-       width = 18, height = 11, limitsize = FALSE)
-} # Function plotting data by response var = met, environmental variable, and log+1 transformtion or not.
-
-
-metVars <- plot_dhm_long %>% select(met.type) %>% unique %>% as.matrix() %>% as.vector() #extracting response vars from dataset
-enviroVars <- c("elev", "rad_march", "slope_dem") #environmental vars
-transVars <- c("log", "none") # with and with log transformation
-allVars <- as_tibble(qpcR:::cbind.na(metVars,enviroVars,transVars))
-allVars <- allVars %>% tidyr::expand(metVars, enviroVars, transVars) %>% drop_na() # create dataframe with all combination of variables
-
-mapply(enviroPlots, allVars$metVars, allVars$enviroVars, allVars$transVars) #apply all combinations to graphing function... damn, thats a lot of graphs...
-
-
-
-########################################################################################
-#################################   TRASH   ############################################
-########################################################################################
-
+#####################################################################################
+#####################################################################################
+#  ___/-\___  #                  __                       .__                       #
+# |---------| #                _/  |_____________    _____|  |__                    #
+#  |   |   |  #                \   __\_  __ \__  \  /  ___/  |  \                   #
+#  | | | | |  #                 |  |  |  | \// __ \_\___ \|   Y  \                  #
+#  | | | | |  #                 |__|  |__|  (____  /____  >___|  /                  #
+#  | | | | |  #                                  \/     \/     \/                   #
+#  |_______|  #######################################################################
+#####################################################################################
 
 plot_dhm %>%  filter(Type == "internal", fire_code != "E") #search for plots that are coded internal that arent in Cw site
 
--- scripts/field-data-analysis/field_data__explore_plots__planting_time_since_fire.R
+++ scripts/field-data-analysis/field_data__explore_plots__planting_time_since_fire.R
@@ -1,31 +1,20 @@
 library(tidyverse)
 library(lme4)
+library(ggplot2)
 library(gridExtra)
 
 
 
-
-
-
-## plot distribution of response vars by fire and treated status
-explore <- ggplot(plot_dhm_long, aes(x=interaction(Type, Fire), y=log(value+1),color=Type)) +
-  geom_violin(size = 1) +
-  #geom_boxplot(width=0.1) +
-  #geom_jitter(width = .02, alpha = .25) +
-  geom_dotplot(binaxis='y', method = "histodot", stackdir='center',binwidth = .2, dotsize=.7,  aes(x = interaction(Type, Fire), fill = Type)) +
-  #geom_point(position=position_jitterdodge(dodge.width=0.5,jitter.width=0.15),size=3) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme_bw(12) +
-  facet_wrap(~metric,scales="free")
-ggsave(explore, file = "C:/Users/Quinn Sorenson/Desktop/lotsOplots.pdf", width = 60, height = 40, limitsize = FALSE)
+load("output/plotSeedlingData.RData") #load R object: plot_dhm_long
 
 
 ##### Create function to plot vars and apply to all var combinations -------------------------------------------------
 
 enviroPlots <- function(met, enviro, trans = "none") {
-  assign(paste0(enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met }), ggplot(plot_dhm_long %>% filter(met.type == met), 
-                                                                                                    aes(x = get(enviro), y = if (trans == "log") { log(value+1) } 
-                                                                                                        else { value }, color = fsplanted)) +
+  assign(paste0(enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met }), 
+         ggplot(plot_dhm_long %>% filter(met.type == met), 
+                aes(x = get(enviro), y = if (trans == "log") { log(value+1) } 
+                    else { value }, color = fsplanted)) +
            geom_smooth(method = "lm", size = 1.5) +
            geom_point(size = 2) +
            ylab(if (trans == "log") { paste0("log.", met) } else { met }) +
@@ -38,13 +27,36 @@ enviroPlots <- function(met, enviro, trans = "none") {
          width = 18, height = 11, limitsize = FALSE)
 } # Function plotting data by response var = met, environmental variable, and log+1 transformtion or not.
 
-
 metVars <- plot_dhm_long %>% select(met.type) %>% unique %>% as.matrix() %>% as.vector() #extracting response vars from dataset
-enviroVars <- c("elev", "rad_march", "slope_dem") #environmental vars
+enviroVars <- c("elev", "rad_march", "slope_dem", "normal_annual_precip") #environmental vars
 transVars <- c("log", "none") # with and with log transformation
 allVars <- as_tibble(qpcR:::cbind.na(metVars,enviroVars,transVars))
 allVars <- allVars %>% tidyr::expand(metVars, enviroVars, transVars) %>% drop_na() # create dataframe with all combination of variables
 
 mapply(enviroPlots, allVars$metVars, allVars$enviroVars, allVars$transVars) #apply all combinations to graphing function... damn, thats a lot of graphs...
 
+cor(plots %>% dplyr::select(elev, rad_march, slope_dem, normal_annual_precip),  use = "complete.obs", method = "pearson")
+
+#####################################################################################
+#####################################################################################
+#  ___/-\___  #                  __                       .__                       #
+# |---------| #                _/  |_____________    _____|  |__                    #
+#  |   |   |  #                \   __\_  __ \__  \  /  ___/  |  \                   #
+#  | | | | |  #                 |  |  |  | \// __ \_\___ \|   Y  \                  #
+#  | | | | |  #                 |__|  |__|  (____  /____  >___|  /                  #
+#  | | | | |  #                                  \/     \/     \/                   #
+#  |_______|  #######################################################################
+#####################################################################################
+
+## plot distribution of response vars by fire and treated status
+explore <- ggplot(plot_dhm_long, aes(x=interaction(fsplanted, Fire), y=log(value+1),color=fsplanted)) +
+  geom_violin(size = 1) +
+  #geom_boxplot(width=0.1) +
+  #geom_jitter(width = .02, alpha = .25) +
+  geom_dotplot(binaxis='y', method = "histodot", stackdir='center',binwidth = .2, dotsize=.7,  aes(x = interaction(fsplanted, Fire), fill = Type)) +
+  #geom_point(position=position_jitterdodge(dodge.width=0.5,jitter.width=0.15),size=3) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme_bw(12) +
+  facet_wrap(~metric,scales="free")
+ggsave(explore, file = "C:/Users/Quinn Sorenson/Desktop/lotsOplots.pdf", width = 60, height = 40, limitsize = FALSE)
 
