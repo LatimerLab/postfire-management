7a1d3f96c0c32783cf08b8a8f4dddd9931a3c438
Quinn Sorenson <qmsorenson@gmail.com>
2019-11-01 08:31:30 -0700

    basic changes

--- scripts/field-data-analysis/field_data__analysis__planting_time_since_fire.R
+++ scripts/field-data-analysis/field_data__analysis__planting_time_since_fire.R
@@ -6,12 +6,25 @@ library(gridExtra)
 library(effects)
 
 load("output/plotSeedlingData.RData") #load R object: plot_dhm_long
+plot_dhm <- plot_dhm %>% mutate(ln.dens.planted = log(dens.planted+1)) %>%
+  mutate(fsplanted = as.factor(fsplanted))
 
-m1 <- lmer(log(dens.planted+1) ~ fsplanted*slope_dem + fsplanted*elev + fsplanted*rad_spring_summer + fsplanted*normal_annual_precip + (1|Fire/PairID), data = plot_dhm)
-summary(m1)
-
-m1 <- lmer(log(dens.conif+1) ~ scale(elev)*scale(slope_dem) + fsplanted + scale(normal_annual_precip) + (1|Fire/PairID), data = plot_dhm)
+m1 <- lmer(ln.dens.planted ~ scale(slope_dem) + scale(elev) + 
+             fsplanted*scale(normal_annual_precip) + (fsplanted*scale(normal_annual_precip)|Fire) + (1|Fire:PairID), data = plot_dhm)
+AIC(m1)
 summary(m1)
 plot(m1)
 plot(allEffects(m1))
 
+m2 <- lmer(log(dens.planted+1) ~ scale(elev)*scale(slope_dem) + fsplanted + scale(normal_annual_precip) + (scale(elev)*scale(slope_dem)|Fire) + (1|Fire:PairID), data = plot_dhm)
+summary(m2)
+plot(m2)
+plot(allEffects(m2))
+
+m2 <- lmer(log(dens.planted+1) ~ scale(elev)+scale(slope_dem) + fsplanted + scale(normal_annual_precip) + (scale(normal_annual_precip)|Fire) + (1|Fire:PairID), data = plot_dhm)
+summary(m2)
+AIC(m2)
+plot(m1)
+plot(allEffects(m1))
+
+m3 <- glmer(
--- scripts/field-data-analysis/field_data__explore_plots__planting_time_since_fire.R
+++ scripts/field-data-analysis/field_data__explore_plots__planting_time_since_fire.R
@@ -35,8 +35,45 @@ allVars <- allVars %>% tidyr::expand(metVars, enviroVars, transVars) %>% drop_na
 
 mapply(enviroPlots, allVars$metVars, allVars$enviroVars, allVars$transVars) #apply all combinations to graphing function... damn, thats a lot of graphs...
 
-cor(plots %>% dplyr::select(elev, rad_march, slope_dem, normal_annual_precip),  use = "complete.obs", method = "pearson")
+cor(plots %>% filter(Fire == "AmRiv") %>% dplyr::select(elev, rad_spring_summer, slope_dem, normal_annual_precip),  use = "complete.obs", method = "pearson")
 
+##### Create plots with each fire in on panel -------------------------------------------------------------------------
+
+
+enviroPlots <- function(met, enviro, trans = "none") {
+  assign(paste0(enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met }), 
+         ggplot(plot_dhm_long %>% filter(met.type == met), 
+                aes(x = get(enviro), y = if (trans == "log") { log(value+1) } 
+                    else { value }, color = fsplanted)) +
+           geom_smooth(method = "lm", size = 1.5) +
+           geom_point(size = 2) +
+           ylab(if (trans == "log") { paste0("log.", met) } else { met }) +
+           scale_color_manual(values = c("#D5C332", "#048BA8")) +
+           theme(plot.title = element_text(hjust = 0.5)) +
+           theme_bw(12) +
+           facet_wrap(~metric, scales="free"))
+  ggsave(get(paste0(enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met })), 
+         file = paste0("figures/exploratory/", enviro, ".plot.", if (trans == "log") { paste0("log.", met) } else { met }, ".pdf"), 
+         width = 18, height = 11, limitsize = FALSE)
+} # Function plotting data by response var = met, environmental variable, and log+1 transformtion or not.
+
+ggplot(plot_dhm_long %>% filter(metric %in% c("dens.planted", "dens.conif")), 
+       aes(x = rad_spring_summer, y =log(value+ 1), shape = Fire, color =Fire)) +
+  geom_smooth(method = "lm", size = 1.5, se = F, aes(linetype = fsplanted)) +
+  geom_point(size = 2) +
+  #ylab(if (trans == "log") { paste0("log.", met) } else { met }) +
+  scale_color_manual(values = c("#89D870", "#B6C61A", "#D8A800", "#006344", "#BD3B1B")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme_bw(12) +
+  facet_wrap(~metric, scales="free")
+
+metVars <- plot_dhm_long %>% select(met.type) %>% unique %>% as.matrix() %>% as.vector() #extracting response vars from dataset
+enviroVars <- c("elev", "rad_spring_summer", "slope_dem", "normal_annual_precip") #environmental vars
+transVars <- c("log", "none") # with and with log transformation
+allVars <- as_tibble(qpcR:::cbind.na(metVars,enviroVars,transVars))
+allVars <- allVars %>% tidyr::expand(metVars, enviroVars, transVars) %>% drop_na() # create dataframe with all combination of variables
+
+mapply(enviroPlots, allVars$metVars, allVars$enviroVars, allVars$transVars) #apply all combinations to graphing function... damn, thats a lot of graphs...
 
 
 
