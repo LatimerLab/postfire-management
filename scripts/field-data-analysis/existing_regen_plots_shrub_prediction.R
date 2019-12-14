14b2e147571d4bd92bed6591acf3fcf8e2a32646
Unknown <derek.j.n.young@gmail.com>
2019-12-13 19:17:08 -0600

    Some basic shrub modeling

--- scripts/field-data-analysis/existing_regen_plots_shrub_prediction.R
+++ scripts/field-data-analysis/existing_regen_plots_shrub_prediction.R
@@ -1,19 +1,34 @@
 setwd("~/Research Projects/Post-fire management/postfire-management")
 
 library(tidyverse)
-library(betareg)
 library(brms)
 
+### Load the data
 d = read.csv("existing_regen/regen_plots_w_gis_data.csv",stringsAsFactors = FALSE) %>%
   mutate(Fire_and_Age = paste(Fire,survey_years_post,sep="_"))
 
 
-##!! Temporary: remove the shrub plots with exactly 0% and 100% cover. TODO: Transform 0 and 100 to be off the boundary.
+###!!! Temporary: remove the shrub plots with exactly 0% and 100% cover. TODO: Transform 0 and 100 to be off the boundary.
 d = d %>%
   filter(SHRUB != 0 & SHRUB != 100) %>%
   mutate(SHRUB = SHRUB/100)
 
+### Run beta regression in BRMS
+delta_val = 0.95
+treedepth = 12
+m = brm(SHRUB ~ scale(normal_annual_precip) * scale(tmax) + scale(tpi5000) + (1|Fire_and_Age) + scale(rad_winter), data=d, family ="beta",chains=3,cores=3,control=list(adapt_delta=delta_val,max_treedepth=treedepth),seed=5, inits=0)
+
+summary(m)
+
+# Compare fitted vs. observed
+
+fitted = fitted(m)[,"Estimate"]
+observed = d$SHRUB
+
+plot(observed,fitted) # < not great looking
+
+cor(observed,fitted)^2  # rough approximate R-sq of 0.22
 
 
-m = brm(SHRUB ~ normal_annual_precip + (1|Fire_and_Age), data=d, family ="beta",chains=3,cores=3)
+
 
