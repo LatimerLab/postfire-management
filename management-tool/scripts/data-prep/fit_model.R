05eafc191e5b9a3971896a94c892ba56c1c5c96e
Unknown <derek.j.n.young@gmail.com>
2020-02-18 21:55:01 -0800

    Spatial model predictions and prepping prediction data for shiny

--- management-tool/scripts/data-prep/fit_model.R
+++ management-tool/scripts/data-prep/fit_model.R
@@ -1,7 +1,9 @@
-setwd("~/Research Projects/Post-fire management/postfire-management")
+setwd("~/projects/Post-fire management/postfire-management")
 
 library(tidyverse)
 library(lme4)
+library(sf)
+library(raster)
 
 load("output/plotSeedlingData.RData") #load R object: plot_dhm_long
 plot_dhm <- plot_dhm %>% 
@@ -18,7 +20,7 @@ plot_dhm <- plot_dhm %>%
   mutate(totalCov = Shrubs + Grasses + Forbs) %>%
   mutate(totalCovxHt = (Shrubs*ShrubHt + Grasses*GrassHt + Forbs*ForbHt))
 
-test <- plot_dhm %>% select(Fire, facts.planting.first.year)
+test <- plot_dhm %>% dplyr::select(Fire, facts.planting.first.year)
 unique(test)
 
 ##### final models for planted species and conifers --------------------------------------------------------------------------------
@@ -38,17 +40,59 @@ hist(plot_dhm$neglog5SeedWallConifer,nclass = 20)
 #### Predict to a landscape ####
 
 ## trial landscape subset ##
+region = st_read("management-tool/data/focal-region/focal-region.geojson")
+
+tpi = raster("management-tool/data/non-synced/tpi2000.tif")
+ppt = raster("management-tool/data/non-synced/ppt.tif")
+tmean = raster("management-tool/data/non-synced/tmean.tif")
+
+env = stack(tpi,ppt,tmean*100)
+enc = crop(env,region %>% st_transform(projection(env)))
+enc = mask(enc,region %>% st_transform(projection(env)))
+
+
+env_crop = crop(env,region %>% st_transform(projection(env)))
+
+writeRaster(enc,"management-tool/data/non-synced/preds_stack.tif",overwrite=TRUE, datatype="INT2S", options="COMPRESS=LZW")   ##738
+
+env2 = raster("management-tool/data/non-synced/preds_stack.tif")
+env_crop = crop(env,region %>% st_transform(projection(env)))
+
+
+
+### Extract raster cells to data frame ###
+
+env_df = as.data.frame(env,xy=TRUE)
+
+env_df = env_df %>%
+  mutate(planting_year = 1,
+         planted = FALSE,
+         seedwall = mean(plot_dhm$neglog5SeedWallConifer),
+         Shrubs = asin(sqrt(mean(plot_dhm$Shrubs/100))),
+         ShrubHt = mean(plot_dhm$ShrubHt))
+
+env_df = env_df %>%
+  rename(tpi2000 = tpi2000,
+         normal_annual_precip = ppt,
+         tmean = tmean,
+         Shrubs = Shrubs,
+         ShrubHt = ShrubHt,
+         neglog5SeedWallConifer = seedwall,
+         facts.planting.first.year = planting_year,
+         fsplanted = planted)
 
 
 
 
+####
 
+preds = predict(pltd,env_df,re.form=NA)
 
+env_df$preds = preds
 
+pred_raster = rasterFromXYZ(env_df %>% dplyr::select(x,y,preds))
 
-
-
-
+plot(pred_raster)
 
 
 
