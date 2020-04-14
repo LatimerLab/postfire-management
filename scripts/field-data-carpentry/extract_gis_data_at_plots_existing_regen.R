fb896a8af925ca90c56b43b9f815e94e38d9b058
Derek <derek.j.n.young@gmail.com>
2020-04-13 18:46:29 -0700

    Extract downscaled temperature for existing regen plots

--- scripts/field-data-carpentry/extract_gis_data_at_plots_existing_regen.R
+++ scripts/field-data-carpentry/extract_gis_data_at_plots_existing_regen.R
@@ -1,4 +1,6 @@
-setwd("C:/Users/DYoung/Documents/Research Projects/Post-fire management/postfire-management")
+setwd("/home/derek/repos/postfire-management")
+
+
 
 library(tidyverse)
 library(raster)
@@ -9,7 +11,7 @@ library(dynatopmodel)
 
 ## load data
 plots = read.csv("existing_regen/plots_compiled_filtered.csv",stringsAsFactors = FALSE)
-plots = read.csv("data/field-processed/compiled-processed/plots.csv",stringsAsFactors = FALSE)
+#plots = read.csv("data/field-processed/compiled-processed/plots.csv",stringsAsFactors = FALSE)
 
 
 ### !! For plots in UTM 10N
@@ -24,13 +26,13 @@ plots_sp <- st_as_sf(plots, coords = c("Easting","Northing"), crs = 26910) %>%
 
 ### For plots in geographic
 ## make plots spatial
-
-plots = plots %>%
-  mutate(Long = as.numeric(Long)*-1,
-         Lat = as.numeric(Lat))
-
-plots_sp <- st_as_sf(plots, coords = c("Long","Lat"), crs = 4326) %>%
-  st_transform(3310)
+# 
+# plots = plots %>%
+#   mutate(Long = as.numeric(Long)*-1,
+#          Lat = as.numeric(Lat))
+# 
+# plots_sp <- st_as_sf(plots, coords = c("Long","Lat"), crs = 4326) %>%
+#   st_transform(3310)
 
 
 
@@ -42,9 +44,9 @@ ppt = raster("data/non-synced/existing-datasets/precipitation/PRISM_ppt_30yr_nor
 plots_sp$normal_annual_precip = raster::extract(ppt,plots_sp,method="bilinear")
 
 ## extract TopoWX normal temperature
-tmax = raster("data/non-synced/existing-datasets/topowx_temerature/tmax_normal/normals_tmax.nc")
-tmin = raster("data/non-synced/existing-datasets/topowx_temerature/tmin_normal/normals_tmin.nc")
-tmean = mean(tmax,tmin)
+tmax = raster("data/non-synced/existing-datasets/topowx_temerature/tmax_ds.tif")
+tmin = raster("data/non-synced/existing-datasets/topowx_temerature/tmin_ds.tif")
+tmean = (tmax + tmin)/2
 plots_sp$tmax = raster::extract(tmax,plots_sp,method="bilinear")
 plots_sp$tmin = raster::extract(tmin,plots_sp,method="bilinear")
 plots_sp$tmean = raster::extract(tmean,plots_sp,method="bilinear")
@@ -59,8 +61,10 @@ dem_local = crop(dem,plots_vicinity %>% as("Spatial"))
 dem_local = mask(dem_local,plots_vicinity %>% as("Spatial"))
 dem_local = reclassify(dem_local,cbind(0,NA)) # set zero to nodata
 
-template = raster(xmn = -122055,xmx = 199924, ymn = -333167, ymx = 301365, resolution = 30,  crs = "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+template = raster(xmn = -350000,xmx = 350000, ymn = -480000, ymx = 480000, resolution = 30,  crs = "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
 dem_reg = resample(dem_local,template)
+gc()
+dem_reg[dem_reg == 0] = NA
 gc()
 
 topo_data = data.frame()
@@ -77,8 +81,8 @@ for(fire in fires) {
   plots_fire_vicinity = st_buffer(plots_fire,20000) %>% st_union()
   dem_local_fire = crop(dem_local,plots_fire_vicinity %>% as("Spatial"))
   dem_local_fire = mask(dem_local_fire,plots_fire_vicinity %>% as("Spatial"))
-  # dem_reg_fire = crop(dem_reg,plots_fire_vicinity %>% as("Spatial"))
-  # dem_reg_fire = mask(dem_reg_fire,plots_fire_vicinity %>% as("Spatial"))
+  dem_reg_fire = crop(dem_reg,plots_fire_vicinity %>% as("Spatial"))
+  dem_reg_fire = mask(dem_reg_fire,plots_fire_vicinity %>% as("Spatial"))
   
   # filename = paste0("../dem_reg_",fire,".tif")
   # writeRaster(dem_reg_fire,filename,overwrite=TRUE)
@@ -89,15 +93,15 @@ for(fire in fires) {
   tpi2000 = tpi(dem_local_fire,scale = round(2000/30))
   tpi5000 = tpi(dem_local_fire,scale = round(5000/30)) # this takes hours
 
-  # twi = upslope.area(dem_reg_fire,atb=TRUE)
+  twi = upslope.area(dem_reg_fire,atb=TRUE)
 
   tpi500_d = extract(tpi500, plots_fire, method="bilinear")
   tpi100_d = extract(tpi100, plots_fire, method="bilinear")
   tpi2000_d = extract(tpi2000, plots_fire, method="bilinear")
   tpi5000_d = extract(tpi5000, plots_fire, method="bilinear")
-  # twi_d = extract(twi[["atb"]], plots_fire, method="bilinear")
+  twi_d = extract(twi[["atb"]], plots_fire, method="bilinear")
   # 
-  topo_data_fire = data.frame(Regen_Plot = plots_fire$Regen_Plot,tpi100 = tpi100_d, tpi500 = tpi500_d, tpi2000 = tpi2000_d, tpi5000 = tpi5000_d)
+  topo_data_fire = data.frame(Regen_Plot = plots_fire$Regen_Plot,tpi100 = tpi100_d, tpi500 = tpi500_d, tpi2000 = tpi2000_d, tpi5000 = tpi5000_d, twi = twi_d)
   topo_data = bind_rows(topo_data,topo_data_fire)
 
   
@@ -131,7 +135,7 @@ rad_winter_socal = raster("data/non-synced/existing-datasets/solar radiation/rad
 rad_winter_norcal = raster("data/non-synced/existing-datasets/solar radiation/rad_winter_norcal.tif")
 rad_winter = merge(rad_winter_socal,rad_winter_norcal)
 
-rad_winter_spring_socal = raster("data/non-synced/existing-datasets/solar radiation/rad_winter_spring_socal.tif")
+      rad_winter_spring_socal = raster("data/non-synced/existing-datasets/solar radiation/rad_winter_spring_socal.tif")
 rad_winter_spring_norcal = raster("data/non-synced/existing-datasets/solar radiation/rad_winter_spring_norcal.tif")
 rad_winter_spring = merge(rad_winter_spring_socal,rad_winter_spring_norcal)
 
