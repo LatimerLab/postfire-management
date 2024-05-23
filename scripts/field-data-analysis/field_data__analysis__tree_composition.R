e2f5f5b1867d29cf30cb9de83f7db68f8987dd90
Andrew Latimer <amlatimer@ucdavis.edu>
2024-05-23 16:44:11 -0700

    minor changes to old proportion pine script

--- scripts/field-data-analysis/field_data__analysis__tree_composition.R
+++ scripts/field-data-analysis/field_data__analysis__tree_composition.R
@@ -20,8 +20,8 @@ plot_dhm %>% group_by(Fire) %>%
 plot_dhm_pine <- plot_dhm
 #plot_dhm_pine$p.pine <- cbind(round(plot_dhm$dens.conif/24.94098, 0), round(plot_dhm$dens.pine/24.94098, 0)) 
 plot_dhm_pine <- plot_dhm_pine %>% 
-  filter(dens.conif != 0) %>%
-  mutate(ForShr = Shrubs + Forbs)
+  filter(dens.conif != 0) %>% # look only at plots that contain conifers
+  mutate(ForShr = Shrubs + Forbs) # combine shrubs and forbs
 
 nrow(plot_dhm_pine) # 119 rows -- this is substantially smaller, so may need to refit a simpler model
 nrow(plot_dhm) # 182 rows 
@@ -29,17 +29,30 @@ nrow(plot_dhm) # 182 rows
 vars_to_test_continuous <- c("tmax", "tmean", "tmin", "normal_annual_precip", "rad_summer", "Forbs", "Shrubs", "Grasses", "ShrubHt", "LiveOverstory", "log10SeedWallConifer", "twi", "tpi2000", "elev", "CWD_sound")
 vars_to_test_factor <- c("fsplanted", "facts.planting.first.year")
 
+# Check for missing values by variable 
+check_missing_values(plot_dhm_pine[,vars_to_test_continuous]) # one missing in LiveOverstory
+hist(plot_dhm$LiveOverstory)
+sum(plot_dhm$LiveOverstory==0, na.rm=T)/length(plot_dhm_pine) # This variable is 88% zeros -- can we leave it out? Will leave out for now. 
+
+# Check variable correlations 
+cor(plot_dhm_pine[,vars_to_test_continuous], use = "na.or.complete")
+# Temp variables too strongly correlated, only tmin is less than 0.7 correlated with elevation. Everything else looks ok. 
+
+# Update continuous variables to test in light of correlations and heavy zero-domination of live overstory
+vars_to_test_continuous <- c("tmin", "normal_annual_precip", "rad_summer", "Forbs", "Shrubs", "Grasses", "ShrubHt", "log10SeedWallConifer", "twi", "tpi2000", "elev", "CWD_sound")
+
 # scale continuous variables 
 plot_dhm_pine_std <- stdize(plot_dhm_pine[ , vars_to_test_continuous], prefix = FALSE)
 
 # add factor variables
 plot_dhm_pine_std <- cbind(plot_dhm_pine_std, plot_dhm_pine[, vars_to_test_factor])
 
-# add the response 
+# Sdd the response 
 
-#Binomial version: plot_dhm_pine_std <- mutate(plot_dhm_pine_std, 
-      #seedling_count = round(plot_dhm_pine$dens.conif/24.94098, 0), 
-      #pine_count = round(plot_dhm_pine$dens.pine/24.94098, 0))
+#Binomial version: 
+plot_dhm_pine_std <- mutate(plot_dhm_pine_std, 
+      seedling_count = round(plot_dhm_pine$dens.conif/24.94098, 0), 
+      pine_count = round(plot_dhm_pine$dens.pine/24.94098, 0))
 
 # Continuous [0,1] version for beta regression 
 plot_dhm_pine_std <- mutate(plot_dhm_pine_std, 
@@ -70,6 +83,13 @@ pines.m1
 loo(pines.m1, moment_match = TRUE)
 plot(pines.m1)
 
+# Diagnostic plots
+pines.m1.res <- resid(pines.m1)[,1]
+pines.m1.fitted <- fitted(pines.m1)[,1]
+qqnorm(pines.m1.res) # underfitted 
+plot(pines.m1.res ~ pines.m1.fitted) 
+plot(pines.m1.fitted ~ plot_dhm_pine_std$prop_response) # not awesome, not terrible 
+
 # Only planting is significant -- and has strong positive effect on prop pine. Almost significant interaction with planting year, so that the effect of planting on pine composition is weaker when planting is sooner after fire (presumably because natural pine regen declines later on?). 
 
 # Other suggetive results are: 
@@ -89,7 +109,7 @@ plot(pines.m1)
 #  log10SeedWallConifer + twi + tpi2000 + elev + CWD_sound + facts.planting.first.year + fsplanted + (1|Fire) + (1|Fire:PairID)")
 # beta version  
 fm_formula <- as.formula("prop_response ~ tmin + tmean + normal_annual_precip + rad_summer + Forbs + Shrubs + Grasses + ShrubHt + LiveOverstory + 
-  log10SeedWallConifer + twi + tpi2000 + elev + CWD_sound + facts.planting.first.year + fsplanted + (1|Fire) + (1|Fire:PairID)")
+  log10SeedWallConifer + twi + tpi2000 + elev + CWD_sound + facts.planting.first.year * fsplanted + (1|Fire) + (1|Fire:PairID)")
 
 # Binomial 
 #fm1 <- glmer(fm_formula, data = plot_dhm_pine_std, family = "binomial", na.action = na.fail) # note won't converge 
