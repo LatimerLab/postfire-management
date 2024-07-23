46309f5c1d1c16f166da4ad0c41584d260ae9057
Andrew Latimer <amlatimer@ucdavis.edu>
2024-07-23 12:32:49 -0700

    Add code to create model comparison table

--- scripts/field-data-analysis/field_data_analysis_planting_time_since_fire_2024.R
+++ scripts/field-data-analysis/field_data_analysis_planting_time_since_fire_2024.R
@@ -90,22 +90,6 @@ model_list <- lapply(model_fixed_effects, FUN = fit_lmer_model, response = respo
 
 AIC_vals <- unlist(lapply(model_list, AIC))
 
-# Make a data frame of the AIC values for each model
-
-#First create a row for the base model 
-base_model_info <- data.frame(model = as.character(formula(base_model))[3], AIC = AIC(base_model), delta_AIC = 0)
-
-model_selection_table <- data.frame(model = as.character(unlist(lapply(model_list, formula))), AIC = AIC_vals, delta_AIC = AIC(base_model) - AIC_vals)
-model_selection_table <- rbind(base_model_info, model_selection_table)
-
-model_selection_table <- mutate(model_selection_table,  AIC = round(AIC, 2), delta_AIC = round(delta_AIC, 2))
-
-# Convert to tabular format for Word
-flextable(model_selection_table) %>% 
-  # make columns of the flextable wider
-  flextable::width(j = 1:3, width = 1) %>%
-  save_as_docx(path = "./figures/exploratory/mytable.docx")
-
 which((AIC(base_model) - AIC_vals) >= 2) # Models 13, 20, and 21 have lower AIC than the base model
 model_list[[13]] # tmin:normal_annual_precip
 model_list[[20]]# tpi2000:elev
@@ -119,24 +103,21 @@ vars_to_test_3way <- c(vars_to_test, "tmin:normal_annual_precip", "tpi2000:elev"
 base_model_2way <- fit_lmer_model(x = vars_to_test_3way, response = response_variable, groups = groups, data = plot_dhm_for_model) 
 
 # Create a list of candidate 3-way interactions 
-model_fixed_effects_3way <- list()
-for (i in 1:length(three_way_interacs_to_test)) {
-  model_fixed_effects_3way[[i]] <- c(vars_to_test_3way, two_way_interacs_to_test[i],  three_way_interacs_to_test[i])
-}
+#model_fixed_effects_3way <- list()
+#for (i in 1:length(three_way_interacs_to_test)) {
+#  model_fixed_effects_3way[[i]] <- c(vars_to_test_3way, two_way_interacs_to_test[i], # three_way_interacs_to_test[i])
+#}
 
 # Fit all the models with 3-way interactions to test
-model_list_3way <- lapply(model_fixed_effects_3way, FUN = fit_lmer_model, response = response_variable, groups = groups, data = plot_dhm_for_model)
+#model_list_3way <- lapply(model_fixed_effects_3way, FUN = fit_lmer_model, response = response_variable, groups = groups, data = plot_dhm_for_model)
 
-AIC_vals_3way <- unlist(lapply(model_list_3way, AIC))
-
-which((AIC(base_model_2way) - AIC_vals_3way) >= 2) # Shrub interaction improves AIC. 
-
-## Direct comparison of models with different interactions involving shrubs 
-# base_model_2way has no shrub interactions 
+## Directly compare of models with different interactions involving shrubs 
+  # base_model_2way has no shrub interactions
+  # base_model_2way_shrubs has a 2-way interaction with shrubs and planting
+  # base_model_3way_shrubs has a 3-way interaction with shrubs, planting, and planting timing. 
 base_model_2way_shrubs <- fit_lmer_model(x = c(vars_to_test_3way, "Shrubs:fsplanted"), response = response_variable, groups = groups, data = plot_dhm_for_model)
 base_model_3way_shrubs <- fit_lmer_model(x = c(vars_to_test_3way, "Shrubs:fsplanted", "Shrubs:fsplanted:facts.planting.first.year"), response = response_variable, groups = groups, data = plot_dhm_for_model)
 AIC(base_model_2way, base_model_2way_shrubs, base_model_3way_shrubs) # 3-way interaction improves AIC
-
 
 #### Next and final step: Backward stepwise elimination of variables using AIC ####
 
@@ -182,3 +163,27 @@ plot(simulationOutput) # Residuals qqplot and distribution are fine; quantile de
 
 save(seedling_density_final_model, file = "./output/seedling_density_final_model.Rdata")
 
+
+
+#### Make table of model selection procedure, including AIC for all models considered
+
+#First create a row for the base model 
+base_model_info <- data.frame(model = as.character(formula(base_model))[3], AIC = AIC(base_model), delta_AIC = 0)
+
+# Add the models testing 2-way interactions
+model_selection_table <- data.frame(model = as.character(unlist(lapply(model_list, formula))), AIC = AIC_vals, delta_AIC = AIC(base_model) - AIC_vals)
+model_selection_table <- rbind(base_model_info, model_selection_table)
+
+# Add the model with the 3-way interaction 
+model_selection_table <- rbind(model_selection_table, data.frame(model = as.character(formula(base_model_3way_shrubs))[3], AIC = AIC(base_model_3way_shrubs), delta_AIC = AIC(base_model) - AIC(base_model_3way_shrubs)))
+
+# Add models tested for backwards variable elimination 
+model_list_backwards <- list(m2$model_list[[6]], m3$model_list[[2]], m4$model_list[[1]], m5$model_list[[4]], m6$model_list[[1]], m7$model_list[[1]])
+model_selection_table <- rbind(model_selection_table, data.frame(model = as.character(unlist(lapply(model_list_backwards, formula))), AIC = unlist(lapply(model_list_backwards, AIC)), delta_AIC = AIC(base_model) - unlist(lapply(model_list_backwards, AIC))))
+
+##### Convert model selection table to tabular format for Word and export 
+model_selection_table <- mutate(model_selection_table,  AIC = round(AIC, 2), delta_AIC = round(delta_AIC, 2))
+flextable(model_selection_table) %>% 
+  # make columns of the flextable wider
+  flextable::width(j = 1:3, width = 1) %>%
+  save_as_docx(path = "./figures/exploratory/seedling_density_model_comparison_table.docx")
